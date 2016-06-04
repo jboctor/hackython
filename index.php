69aa88a082bbe65dc65b742b8e5ee73fc8d67d5a
John Boctor <jboctor@outlook.com>
2016-06-04 14:09:40 -0500

    Added jquery and script to log key presses

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 ?>
 <html>
     <head>
+        <script type="text/javascript" src="jquery-2.2.4.min.js"></script>
+        <script type="text/javascript">
+            $.keypress(function (event) {
+                console.log(event);
+            });
+        </script>
         <title>
         </title>
     </head>
