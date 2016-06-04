6f527f8c7da6b1f4c424b4c776abf13624787423
John Boctor <jboctor@outlook.com>
2016-06-04 14:37:43 -0500

    Placed a div on keypress

--- index.php
+++ index.php
@@ -6,7 +6,11 @@
         <script type="text/javascript" src="jquery-2.2.4.min.js"></script>
         <script type="text/javascript">
             $(function () {
+                var current_char = "0";
                 $(window).on("keypress", function (event) {
+                    div = $("<div>", {id: "div_" + count, class: "click " + current_char});
+                    div.innerText(current_char);
+                    $(window).append(div);
                     console.log(event);
                 });
                 $(window).on("click", function (event) {
